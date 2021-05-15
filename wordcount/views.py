e6b52d35fd12fedacc36a36d1bb48863220eaae1
unknown <rajay230401@gmail.com>
2021-05-15 21:45:22 +0530

    Modified

--- wordcount/views.py
+++ wordcount/views.py
@@ -7,6 +7,13 @@ def words(request):
     return render(request,'words.html')
 def count(request):
     fulltext=request.GET['fulltext']
+    count=0
+    punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    for i in range(len(fulltext)):
+        count+=1
+    for ele in fulltext: 
+        if ele in punc: 
+            fulltext = fulltext.replace(ele, "")
     wordlist=fulltext.split()
     words={}
     for word in wordlist:
@@ -15,6 +22,6 @@ def count(request):
         else:
             words[word]+=1
     dictionary=sorted(words.items(),reverse=False)
-    return render(request,'count.html',{'fulltext':fulltext,'count':len(wordlist),'worddict':dictionary})
+    return render(request,'count.html',{'fulltext':fulltext,'count':len(wordlist),'worddict':dictionary,'counter':count})
 def about(request):
     return render(request,'about.html')
