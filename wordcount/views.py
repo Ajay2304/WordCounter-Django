ed7ebe525b945026a544a9dcf5633b1e374ab6fd
AjayRamesh2304 <rajay230401@gmail.com>
2023-11-12 00:40:17 +0530

    Refactoring word count project

--- wordcount/views.py
+++ wordcount/views.py
@@ -8,12 +8,12 @@ def words(request):
 def count(request):
     fulltext=request.GET['fulltext']
     count=0
-    punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
     for i in range(len(fulltext)):
         count+=1
-    for ele in fulltext: 
-        if ele in punc: 
-            fulltext = fulltext.replace(ele, "")
+    for element in fulltext: 
+        if element in punctuations: 
+            fulltext = fulltext.replace(element, "")
     wordlist=fulltext.split()
     words={}
     for word in wordlist:
@@ -22,6 +22,6 @@ def count(request):
         else:
             words[word]+=1
     dictionary=sorted(words.items(),reverse=False)
-    return render(request,'count.html',{'fulltext':fulltext,'count':len(wordlist),'worddict':dictionary,'counter':count})
+    return render(request,'count.html',{'fulltext':fulltext,'count':len(wordlist),'worddictionary':dictionary,'counter':count})
 def about(request):
     return render(request,'about.html')
